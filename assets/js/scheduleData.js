a78107893262e00fe63d1b4bc7508c752d239a50
Salocin.TEN <salocin.ten@gmail.com>
2016-03-14 09:34:51 +0800

    Prep schedule for the final data input.
    Either this one or we just iFrame the google docs spreadsheet.

--- assets/js/scheduleData.js
+++ assets/js/scheduleData.js
@@ -6,20 +6,17 @@ geekcampmy2016.controller('ScheduleController', function () {
     //Guys, please fill out the proper track 1 schedule here. Note the field names have to be intact,
     //even if there is no value. One curly bracket row is one table row...
     var track1schedule = [
-        {timeslot: "8 a.m.", speaker: "", title: "Registration & Networking"},
-        {timeslot: "9 a.m.", speaker: "Kevin Tan", speakerUrl:"https://about.me/kevintanhongann", title: "TBA"},
-        {timeslot: "10 a.m.",
-         speaker: "Ruben Tan",
-         speakerDesc: "Ruben Tan lived through the dark ages of Javascript and is currently a co-founder and primary custodian of the Javascript Developers Malaysia user group. Ex-CTO of Bookya, he is currently figuring out how to upload his consciousness into a robot to live forever.",
-         title: "TBA"},
-        {timeslot: "11 a.m.", speaker: "Khairil Yusof", speakerUrl:"http://sinarproject.org/en/about/team", title: "TBA"},
-        {timeslot: "12 p.m.", speaker: "TBA", speakerUrl: "#", title: "TBA"},
-        {timeslot: "1 p.m.", speaker: "", title: "Lunch break & Networking"},
-        {timeslot: "2 p.m.", speaker: "TBA", speakerUrl: "#", title: "TBA"},
-        {timeslot: "3 p.m.", speaker: "TBA", speakerUrl: "#", title: "TBA"},
-        {timeslot: "4 p.m.", speaker: "", title: "Tea Break & Networking"},
-        {timeslot: "5 p.m.", speaker: "TBA", speakerUrl: "#", title: "TBA"},
-        {timeslot: "6 p.m.", speaker: "TBA", speakerUrl: "#", title: "TBA"}
+        {timeslot: "8 a.m.", speaker: " ", title: "Introductions"},
+        {timeslot: "9 a.m.", speaker: " ", title: " "},
+        {timeslot: "10 a.m.", speaker: " ", title: " "},
+        {timeslot: "11 a.m.", speaker: " ", title: " "},
+        {timeslot: "12 p.m.", speaker: " ", title: " "},
+        {timeslot: "1 p.m.", speaker: "", title: " "},
+        {timeslot: "2 p.m.", speaker: " ", title: " "},
+        {timeslot: "3 p.m.", speaker: " ", title: " "},
+        {timeslot: "4 p.m.", speaker: " ", title: "Tea Break & Networking"},
+        {timeslot: "5 p.m.", speaker: " ", title: " "},
+        {timeslot: "6 p.m.", speaker: " ", title: " "}
     ];
     
     scheduler.slots = track1schedule; //Load by default the track 1 schedule.
